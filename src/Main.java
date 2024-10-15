d7a18ae702f456ca7a6ff637bea540ea64dce56a
AdamRitzler <adam.ritzler@gmail.com>
2024-10-15 09:56:31 +0200

    la till en kommentar

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+/*
+skapa en fil reader som läser av en fil och sedan tar från en input och jämför inputen med filen.
+ */
 
     }
 }
