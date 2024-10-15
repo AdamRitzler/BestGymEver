480d92f96175af8ccee8d9f0f800445d2a2aa9b9
AdamRitzler <adam.ritzler@gmail.com>
2024-10-15 09:59:01 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 /*
 skapa en fil reader som läser av en fil och sedan tar från en input och jämför inputen med filen.
  */
+        Scanner scan = new Scanner(System.in);
+        String text;
+        System.out.println("skriv något:");
+        while (scan.hasNextLine()) {
+            text = scan.nextLine();
+
+
+            System.out.println("Du skrev: " + text);
+            System.out.println("skriv något:");
+        }
 
     }
 }
