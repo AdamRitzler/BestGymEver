261fa1b1f1528f459d53581546ae923b35ee920b
AdamRitzler <adam.ritzler@gmail.com>
2024-10-15 10:04:59 +0200

    tog bort test

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,6 @@ public class Main {
 /*
 skapa en fil reader som läser av en fil och sedan tar från en input och jämför inputen med filen.
  */
-        Scanner scan = new Scanner(System.in);
-        String text;
-        System.out.println("skriv något:");
-        while (scan.hasNextLine()) {
-            text = scan.nextLine();
-
-
-            System.out.println("Du skrev: " + text);
-            System.out.println("skriv något:");
-        }
 
     }
 }
